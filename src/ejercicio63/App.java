83b6fb13060d0c42de19b1be09ad4bf65b6919f6
Juan A. García <juan.a.garcia.gomez@gmail.com>
2025-11-12 13:52:58 +0100

    cambio 63 con fondo y contenido

--- src/ejercicio63/App.java
+++ src/ejercicio63/App.java
@@ -1,15 +1,15 @@
 package ejercicio63;
 
 public class App {
-    public static String pintaFilaDePiramide(int altura, int fila){
+    public static String pintaFilaDePiramide(int altura, int fila, char fondo, char contenido){
         String resultado = "";
         if(fila>=1){
-            resultado += pintaCaracteres(altura-fila, ' ');            
-            resultado += pintaCaracteres(2*fila-1, '*');                        
-            resultado += pintaCaracteres(altura-fila, ' ');
+            resultado += pintaCaracteres(altura-fila, fondo);            
+            resultado += pintaCaracteres(2*fila-1, contenido);                        
+            resultado += pintaCaracteres(altura-fila, fondo);
         }
         else{
-            resultado += pintaCaracteres(2*altura-1, ' ');        
+            resultado += pintaCaracteres(2*altura-1, fondo);        
         }
         return resultado;
     }
@@ -21,23 +21,25 @@ public class App {
         return resultado;
     }
     public static void main(String[] args) {
-        int alt1 = 3;
+        int alt1 = 10;
         int alt2 = 10;
         int espacios = 0;
-        int paso = -1;        
+        int paso = 1;        
+        char fondo = '░';
+        char contenido = '█';
         
         for(int i=(paso>0)?1:Math.max(alt1, alt2);
             (paso>0)?i<=Math.max(alt1, alt2):i>=0;
             i+=paso){
             if(alt1>=alt2){
-                System.out.print(pintaFilaDePiramide(alt1, i));
-                System.out.print(pintaCaracteres(espacios, ' '));
-                System.out.print(pintaFilaDePiramide(alt2, i-(alt1-alt2)));                
+                System.out.print(pintaFilaDePiramide(alt1, i, fondo, contenido));
+                System.out.print(pintaCaracteres(espacios, fondo));
+                System.out.print(pintaFilaDePiramide(alt2, i-(alt1-alt2), fondo, contenido));                
             }
             else{
-                System.out.print(pintaFilaDePiramide(alt1, i-(alt2-alt1)));
-                System.out.print(pintaCaracteres(espacios, ' '));
-                System.out.print(pintaFilaDePiramide(alt2, i));
+                System.out.print(pintaFilaDePiramide(alt1, i-(alt2-alt1), fondo, contenido));
+                System.out.print(pintaCaracteres(espacios, fondo));
+                System.out.print(pintaFilaDePiramide(alt2, i, fondo, contenido));
             }
             System.out.println();
         }
